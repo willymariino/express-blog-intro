fa53e27abe480e3da013f0c2207faf91c7776517
willymariino <wg.mariino@gmail.com>
2025-03-26 17:03:59 +0100

    feat: added route

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const express = require("express") // vado ad importare express
 const app = express() // invoco express
 const port = 3000 // indico quale porta voglio utilizzare
 
+// aggiungo una rotta
+app.get("/", (req, res) => { // chiamo la funzione tramite metodo get
+    console.log("sto chiamando la rotta principale")
+})
+
 app.listen(port, () => {
     console.log("sono un server attivo sulla porta:" + " " + port)
 
