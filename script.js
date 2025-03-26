ae51cd33711d0e56bbf2392ac2fa6640ae3afc3f
willymariino <wg.mariino@gmail.com>
2025-03-26 16:43:12 +0100

    added app listen

---
+++ script.js
@@ -0,0 +1,9 @@
+const express = require("express") // vado ad importare express
+const app = express() // invoco express
+const port = 3000 // indico quale porta voglio utilizzare
+
+app.listen(port, () => {
+    console.log("sono un server attivo sulla porta:" + " " + port)
+
+
+})
